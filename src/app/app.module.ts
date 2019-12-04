5c4dc1138521a99ad6699c7bdbe3b870cba36f4e
qburst-george <georgekuttya@qburst.com>
2019-12-04 19:32:39 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { PinchZoomModule } from "ngx-pinch-zoom";
 
 import { AppComponent } from "./app.component";
 import {ImageMapperModule} from "./image-mapper/image-mapper.module";
 
 @NgModule({
-  imports: [BrowserModule, PinchZoomModule, ImageMapperModule],
+  imports: [BrowserModule, ImageMapperModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
